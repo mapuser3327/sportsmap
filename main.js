83d7adc88ba2e4163e87fb4741326a914ef0d609
NJT <nealiet987@gmail.com>
2023-12-08 22:46:54 -0500

    Eagles Roster

--- main.js
+++ main.js
@@ -103,10 +103,29 @@ function init() {
   //   layerControl.addOverlay(stadiumLayer, 'Stadiums');
   // });
 
-  var overlayMaps = {};
+  const playerLayer = L.geoJSON(eaglesPlayers, {
+    onEachFeature: function (feature, layer) {
+      console.log(feature.properties);
+      const html = `<li>${feature.properties.Name} ${feature.properties.EventType}<li>Year: ${feature.properties.EventYear}<li>Position: ${feature.properties.Position}<li>Team: ${feature.properties.Team}<li><a href="https://www.google.com/search?q=nfl+${feature.properties.Team}">Team Data</a>`;
+      layer.bindPopup(html);
+    },
+    pointToLayer: function (feature, latlng) {
+      const nameparts = feature.properties.Name.toLowerCase().split(' ');
+      const lname = nameparts[1] + '_' + nameparts[0];
+      return L.marker(latlng, {
+        icon: getIcon('eaglesProfilePics', lname),
+      });
+    },
+  }).addTo(map);
+
+  var overlayMaps = {
+    'NFL Stadiums': stadiumLayer,
+    'NBA Arenas': arenaLayer,
+    Players: playerLayer,
+  };
 
   const layerControl = L.control
-    .layers(baseMaps, overlayMaps, { collapsed: false })
+    .layers(baseMaps, overlayMaps, { position: 'bottomleft', collapsed: true })
     .addTo(map);
 
   // define functions that right icon for a given feature
@@ -121,47 +140,34 @@ function init() {
   }
 
   // define function to handle click events on metro features
-  function metrosOnEachFeature(feature, layer) {
+  function playersOnEachFeature(feature, layer) {
     layer.on({
       click: function (e) {
         // reset symbol of old selection
-        if (selection) {
-          if (selectedLayer === metroLayer)
-            selection.setIcon(iconByPassday(selectedFeature));
-        }
+        // if (selection) {
+        //   if (selectedLayer === metroLayer)
+        //     selection.setIcon(iconByPassday(selectedFeature));
+        // }
 
         // apply yellow icon to newly selected metro and update selection variables
-        e.target.setIcon(metroSelected);
-        selection = e.target;
-        selectedLayer = metroLayer;
-        selectedFeature = feature;
+        // e.target.setIcon(metroSelected);
+        // selection = e.target;
+        // selectedLayer = metroLayer;
+        // selectedFeature = feature;
 
         // using attributes, construct some HTML to write into the page
-        var featureName = feature.properties.CITY || 'Unnamed feature';
-        var country = feature.properties.COUNTRY || '(Unknown)';
+        var featureName = feature.properties.Name || 'Unnamed feature';
+        var team = feature.properties.Team || '(Unknown)';
         var year = feature.properties.YEAR || '(Unknown)';
-        var passengers = feature.properties.PASSDAY || '(Unknown)';
-        var stations = feature.properties.STATIONS || '(Unknown)';
-        var length = feature.properties.LENGTHKM || '(Unknown)';
+        // var passengers = feature.properties.PASSDAY || '(Unknown)';
+        // var stations = feature.properties.STATIONS || '(Unknown)';
+        // var length = feature.properties.LENGTHKM || '(Unknown)';
         var link = feature.properties.LINK || 'http://www.wikipedia.org';
         var photoHtml =
           feature.properties.PHOTO || '<P>Photo not available</P>';
         var titleHtml =
           '<p style="font-size:18px"><b>' + featureName + '</b></p>';
-        var descripHtml =
-          '<p>The ' +
-          featureName +
-          ', ' +
-          country +
-          ' metro opened in ' +
-          year +
-          ' and currently serves ' +
-          passengers +
-          ' passengers a day. The network consists of ' +
-          stations +
-          ' stations spread over ' +
-          length +
-          ' kilometers.</p>';
+        var descripHtml = '<p>The ' + featureName + ', ' + team + ' </p>';
         var readmoreHtml = '<p><a href="' + link + '">Read more</a></p>';
         document.getElementById('summaryLabel').innerHTML =
           titleHtml + descripHtml + readmoreHtml;
@@ -203,4 +209,33 @@ function init() {
       } else map.removeLayer(lyr);
     });
   };
+
+  displayResults(eagles);
+
+  function displayResults(results) {
+    const listNode = document.getElementById('list_players');
+    const fragment = document.createDocumentFragment();
+    results.features.forEach(function (player, index) {
+      // county.popupTemplate = popupInfo;
+
+      const attributes = player.properties;
+      if (!attributes.player_name) console.log(player);
+      const nameparts = attributes.player.toLowerCase().split(' ');
+      const lname = nameparts[1] + '_' + nameparts[0];
+      const img = `<img src="./eaglesProfilePics/${lname}.webp" width="50" height="35">`;
+      const name = img + '  ' + attributes.player_no + ': ' + attributes.player;
+
+      const li = document.createElement('li');
+      li.classList.add('panel-result');
+      li.tabIndex = 0;
+      li.setAttribute('data-result-id', index);
+      li.setAttribute('unit', attributes.unit);
+      li.setAttribute('position', attributes.position);
+      li.innerHTML = name;
+      fragment.appendChild(li);
+    });
+
+    listNode.innerHTML = '';
+    listNode.appendChild(fragment);
+  }
 }
